42546b38e6606e171b361cc8c9a905b484f5c27e
fabiano-moreira <fabiano.moreira.alves@gmail.com>
2019-11-26 20:21:55 -0300

    Feitas alteraçoes nas views e nas urls do  projeto. Criadas tambem as paginas de login e index e feito processo de autenticaçao

--- projeto_animais/settings.py
+++ projeto_animais/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'projeto_animais.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
